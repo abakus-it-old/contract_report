3bee74747d8744adbc3e0a63a1f8f0e3e37ab216
Valentin Thirion <valentinthirion@gmail.com>
2015-03-13 15:28:57 +0100

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,12 +1,17 @@
 ﻿{
-    'name': "Contract report",
+    'name': "AbAKUS Contract report",
     'version': '1.1',
     'depends': ['sale', 'report'],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Contract',
     'description': """
-    Contract report
+This modules adds the possibility to print service prestation reports for contract for AbAKUS it-solutions.
+
+It also adds a setting wizart for the date selection of prestation range.
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+
     """,
     'data': [
         'reports.xml',
